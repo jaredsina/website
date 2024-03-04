2a74b9dc74fe7dbb5ed879fa88f6f34df94b4941
Sina Hernandez <48105264+jaredsina@users.noreply.github.com>
2024-03-04 11:28:58 -0500

    feat: Add navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+    </div>
+  );
+}
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="row">
+        <a href="" className="navbar-link">
+          Jared Sinai Hernandez
+        </a>
+        <div>
+          <a href="" className="navbar-link">
+            Work
+          </a>
+          <a href="" className="navbar-link">
+            About
+          </a>
+          <a href="" className="navbar-link">
+            Contact
+          </a>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default App;
